7388b0536c044109877524bebcc7698720b5afdc
Leandro y Roberto <leanbazan99@gmail.com>
2023-05-02 11:55:50 -0300

    practica 1-9 terminada

---
+++ algoritmo.js
@@ -0,0 +1,9 @@
+cuerpoPagina= document.querySelector ("body");
+
+function cuerpopagina(){
+    cuerpoPagina.style.color= "black";
+    cuerpoPagina.style.background="white";
+    cuerpoPagina.style.font.family = "arial";
+    cuerpoPagina.style.font.size= "35px";
+}
+    
